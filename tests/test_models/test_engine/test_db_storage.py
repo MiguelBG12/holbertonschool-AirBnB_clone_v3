9606d7233de0a65d625f9a0ff761680c07fb3af6
MiguelBG12 <miguel.bg12@gmail.com>
2024-01-27 17:48:44 -0500

    Test is added for the count function

--- tests/test_models/test_engine/test_db_storage.py
+++ tests/test_models/test_engine/test_db_storage.py
@@ -103,3 +103,16 @@ class TestNewMethodsDb(unittest.TestCase):
         self.assertIs(None, models.storage.get("State", "blah"))
         self.assertIs(None, models.storage.get("blah", "blah"))
         self.assertIs(new_user, models.storage.get("User", new_user.id))
+
+    @unittest.skipIf(os.getenv('HBNB_TYPE_STORAGE') != 'db',
+                     "not testing db storage")
+    def test_count(self):
+        """test that new adds an object to the database"""
+        initial_count = models.storage.count()
+        self.assertEqual(models.storage.count("Blah"), 0)
+        new_state = State(name="California")
+        new_state.save()
+        new_user = User(email="glen@foobar.com", password="password")
+        new_user.save()
+        self.assertEqual(models.storage.count("State"), initial_count + 1)
+        self.assertEqual(models.storage.count(), initial_count + 2)
