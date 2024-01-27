e30b24f8ff0cbf4c6cc982f829a611ca8d65becb
MiguelBG12 <miguel.bg12@gmail.com>
2024-01-27 14:53:21 -0500

    Applying more direct access to the "id" attribute

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -79,7 +79,7 @@ class DBStorage:
         """Get an object based on its class and id"""
         my_dict = self.all(cls)
         for obj in my_dict.values():
-            if obj['id'] == id:
+            if obj.id == id:
                 return obj
         return None
 
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -73,7 +73,7 @@ class FileStorage:
         """Get an object based on its class and id"""
         my_dict = self.all(cls)
         for obj in my_dict.values():
-            if obj['id'] == id:
+            if obj.id == id:
                 return obj
         return None
 
