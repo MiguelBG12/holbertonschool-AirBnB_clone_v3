7517978174dfccc154d2b76405efdce3f09de59a
MiguelBG12 <miguel.bg12@gmail.com>
2024-01-29 09:19:11 -0500

    The updated version optimizes search through the use of keys, improving code efficiency and clarity

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -70,12 +70,19 @@ class FileStorage:
         self.reload()
 
     def get(self, cls, id):
-        """Get an object based on its class and id"""
-        my_dict = self.all(cls)
-        for obj in my_dict.values():
-            if obj.id == id:
-                return obj
-        return None
+        """
+        Retrieve one object.
+
+        Args:
+            cls (class): The class of the object to retrieve.
+            id (str): The ID of the object to retrieve.
+
+        Returns:
+            object: The object based on the class and its ID, or None if
+            not found.
+        """
+        key = "{}.{}".format(cls.__name__, id)
+        return self.__objects.get(key, None)
 
     def count(self, cls=None):
         """Counts objects in the database"""
