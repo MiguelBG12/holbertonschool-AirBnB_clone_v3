7cfb6c647fe28618fa0c4d842f97b69877509f14
MiguelBG12 <miguel.bg12@gmail.com>
2024-02-14 12:29:40 -0500

    Creates a route /status on the object app_views that returns a JSON

---
+++ api/v1/views/index.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Script that import app_views"""
+from api.v1.views import app_views
+from flask import jsonify
+
+@app_views.route('/status')
+def api_status():
+    """a function to return api status"""
+    return jsonify({"status": "OK"})
