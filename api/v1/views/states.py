f586c72d4e480f30d560f82b9f00d7ca9e6d76ac
MiguelBG12 <miguel.bg12@gmail.com>
2024-02-14 13:36:05 -0500

    Fix POST method

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -45,13 +45,15 @@ def post_state():
     if not request.get_json():
         abort(400, description="Not a JSON")
 
-    if 'name' not in request.get_json():
+    data = request.get_json()
+
+    if 'name' not in data:
         abort(400, description="Missing name")
 
-    data = request.get_json()
     instance = State(**data)
     instance.save()
-    return make_response(jsonify(instance.to_dict()), 201)
+
+    return jsonify(instance.to_dict()), 201
 
 
 @app_views.route("/states/<state_id>", methods=["PUT"])
