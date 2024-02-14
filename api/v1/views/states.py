822c01435589619958a2c1653b5a0818c685388c
MiguelBG12 <miguel.bg12@gmail.com>
2024-02-14 13:14:44 -0500

    Adding route that retrieves a specific state

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -14,3 +14,12 @@ def get_states():
     for state in all_states:
         list_states.append(state.to_dict())
     return jsonify(list_states)
+
+
+@app_views.route("states/<state_id>", methods=["GET"], strict_slashes=False)
+def get_state(state_id):
+    """Retrieves a specific state"""
+    state = storage.get(State, state_id)
+    if not state:
+        abort(404)
+    return jsonify(state.to_dict())
