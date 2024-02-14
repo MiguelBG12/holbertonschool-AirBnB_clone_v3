0d64323da074757c920c8daf8ff6daa4984e0275
MiguelBG12 <miguel.bg12@gmail.com>
2024-02-14 13:56:46 -0500

    Deletes a Amenity object

--- api/v1/views/amenities.py
+++ api/v1/views/amenities.py
@@ -25,3 +25,16 @@ def get_amenity(amenity_id):
         abort(404)
 
     return jsonify(amenity.to_dict())
+
+
+@app_views.route('/amenities/<amenity_id>',
+                 methods=['DELETE'], strict_slashes=False)
+def delete_amenity(amenity_id):
+    amenity = storage.get(Amenity, amenity_id)
+    if not amenity:
+        abort(404)
+
+    storage.delete(amenity)
+    storage.save()
+
+    return make_response(jsonify({}, 200))
