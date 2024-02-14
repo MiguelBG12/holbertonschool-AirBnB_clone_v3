5d06c1cabe1c81a4cc54c6e08548e3a78a31b493
MiguelBG12 <miguel.bg12@gmail.com>
2024-02-14 15:22:07 -0500

    Fix bugs in methods of amenities

--- api/v1/views/amenities.py
+++ api/v1/views/amenities.py
@@ -6,73 +6,69 @@ from models.amenity import Amenity
 from models import storage
 
 
-@app_views.route('/amenities', methods=['GET'], strict_slashes=False)
-def get_amenities():
-    """Retrives the list of ameneties"""
-    all_amenities = storage.all(Amenity).values()
-    list_amenities = []
-    for amenity in all_amenities:
-        list_amenities.append(amenity.to_dict())
-    return jsonify(list_amenities)
+@app_views.route("/amenities", methods=["GET"],
+                 strict_slashes=False)
+def getAmenity():
+    """Retrieves the list of all Amenity objects"""
+    all_amenities = []
+    amenities = storage.all(Amenity)
+    for amenity in amenities.values():
+        all_amenities.append(amenity.to_dict())
+    return jsonify(all_amenities)
 
 
-@app_views.route('/amenities/<amenity_id>',
-                 methods=['GET'], strict_slashes=False)
-def get_amenity(amenity_id):
-    """retrives amenty of objects"""
+@app_views.route("/amenities/<amenity_id>", methods=["GET"],
+                 strict_slashes=False)
+def getAmenityID(amenity_id):
+    """Retrieves a Amenity object: """
     amenity = storage.get(Amenity, amenity_id)
-    if not amenity:
+    if amenity is None:
         abort(404)
-
     return jsonify(amenity.to_dict())
 
 
-@app_views.route('/amenities/<amenity_id>',
-                 methods=['DELETE'], strict_slashes=False)
-def delete_amenity(amenity_id):
+@app_views.route("/amenities/<amenity_id>", methods=["DELETE"],
+                 strict_slashes=False)
+def deleteAmenity(amenity_id):
+    """Deletes a Amenity object"""
     amenity = storage.get(Amenity, amenity_id)
-    if not amenity:
+    if amenity is None:
         abort(404)
-
     storage.delete(amenity)
     storage.save()
-
-    return make_response(jsonify({}, 200))
-
-
-@app_views.route('/amenities', methods=['POST'], strict_slashes=False)
-def post_amenity():
-    """create an amenity"""
-    if not request.get_json():
-        abort(400, description="Not a JSON")
-
-    if 'name' not in request.get_json():
-        abort(400, description="Missing name")
-
-    data = request.get_json()
-    instance = Amenity(**data)
-    instance.save()
-    return make_response(jsonify(instance.to_dict()), 201)
+    return jsonify({}), 200
 
 
-@app_views.route('/amenities/<amenity_id>',
-                 methods=['PUT'], strict_slashes=False)
-def put_amenity(amenity_id):
-    """update an amenity object"""
-    if not request.get_json():
-        abort(400, description="Not a JSON")
+@app_views.route("/amenities", methods=["POST"],
+                 strict_slashes=False)
+def createAmenity():
+    """Creates a Amenity"""
+    amenity = request.get_json()
+    if request.is_json:
+        if "name" not in amenity:
+            return "Missing name", 400
+        objNew = Amenity(**amenity)
+        storage.new(objNew)
+        storage.save()
+        return jsonify(objNew.to_dict()), 201
+    return "Not a JSON", 400
 
-    ignore = ['id', 'created_at', 'updated_at']
 
-    amenity = storage.get(Amenity, amenity_id)
-
-    if not amenity:
-        abort(404)
-
-    data = request.get_json()
-    for key, value in data.items():
-        if key not in ignore:
-            setattr(amenity, key, value)
-    storage.save()
-    return make_response(jsonify(amenity.to_dict()), 200)
-
+@app_views.route("amenities/<amenity_id>", methods=["PUT"],
+                 strict_slashes=False)
+def updateAmenity(amenity_id):
+    """Updates a Amenity object"""
+    amenities = storage.get(Amenity, amenity_id)
+    gitignore = ["id", "created_at", "updated_at", "state_id"]
+    try:
+        data = request.get_json()
+        for k, v in data.items():
+            if k not in gitignore:
+                setattr(amenities, k, v)
+        storage.save()
+        return jsonify(amenities.to_dict()), 200
+    except Exception:
+        if amenities is None:
+            abort(404)
+        if not request.is_json:
+            return "Not a JSON", 400
