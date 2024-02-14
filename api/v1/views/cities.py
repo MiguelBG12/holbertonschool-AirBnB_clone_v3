70ba26e5786bab29f8afe030eecc407559b9ee29
MiguelBG12 <miguel.bg12@gmail.com>
2024-02-14 13:50:49 -0500

    Retrieves the list of all City objects of a State

---
+++ api/v1/views/cities.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""City from states"""
+from api.v1.views import app_views
+from flask import abort, jsonify, make_response, request
+from models import storage
+from models.city import City
+from models.state import State
+
+
+@app_views.route('/states/<state_id>/cities',
+                 methods=['GET'], strict_slashes=False)
+def get_state_cities(state_id):
+    """Retrieves a state object"""
+
+    list_cities = []
+    state = storage.get(State, state_id)
+    if not state:
+        abort(404)
+    for city in state.cities:
+        list_cities.append(city.to_dict())
+    return jsonify(list_cities)
