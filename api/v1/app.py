f498d9c16dae4970ebe545ce46164d1441517d1b
MiguelBG12 <miguel.bg12@gmail.com>
2024-02-14 13:00:30 -0500

    Creates a handler for 404 errors that returns a JSON-formatted 404

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """Script that starts a Flask web"""
-from flask import Flask
+from flask import Flask, jsonify
 from models import storage
 from api.v1.views import app_views
 from os import getenv
@@ -8,6 +8,12 @@ from os import getenv
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
+
+
+@app.errorhandler(404)
+def not_found(error):
+    """Handler for 404 errors"""
+    return jsonify({"error": "Not found"}), 404
 
 
 @app.teardown_appcontext
